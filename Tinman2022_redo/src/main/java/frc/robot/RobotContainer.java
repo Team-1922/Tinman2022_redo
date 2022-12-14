0960e4d17842e5c300976eba41309a2f5da1911f
schindm <schindm@users.noreply.github.com>
2022-12-13 20:28:11 -0500

    We delclared the left and right joystick. We entialized the Drivetrain subsystem. We made a new instence the instence of tank drive.

--- Tinman2022_redo/src/main/java/frc/robot/RobotContainer.java
+++ Tinman2022_redo/src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.TankDrive;
+import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -21,6 +24,10 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+private final Joystick m_rightJoystick = new Joystick(0);
+private final Joystick m_leftJoystick = new Joystick(1);
+private final DriveTrainSubsystem m_DriveTrain = new DriveTrainSubsystem();
+private final TankDrive m_TankDrive = new TankDrive(m_DriveTrain);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- Tinman2022_redo/src/main/java/frc/robot/commands/TankDrive.java
+++ Tinman2022_redo/src/main/java/frc/robot/commands/TankDrive.java
@@ -30,7 +30,9 @@ public class TankDrive extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    Joystick
+  }
 
   // Called once the command ends or is interrupted.
   @Override
